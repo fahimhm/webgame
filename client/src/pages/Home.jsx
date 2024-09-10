c4ca69d6f27e46a5de3331170f25cd9bc05c2a24
fahim.hadi <fahimhadimaula@gmail.com>
2024-09-10 19:47:31 +0700

    created simple homepage

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,8 +1,14 @@
-import React from 'react'
-
 function Home() {
   return (
-    <div className='text-6xl'><h1>Home</h1></div>
+    <div className="font-sans text-gray-900 antialiased bg-[#f8f4f3]">
+      <div className="flex items-center justify-center min-h-screen">
+        <div className="flex flex-col space-y-4 md:flex-row md:space-y-0 md:space-x-4">
+          <button className="px-4 py-2 bg-[#20C997] text-white rounded">Button 1</button>
+          <button className="px-4 py-2 bg-[#20C997] text-white rounded">Button 2</button>
+          <button className="px-4 py-2 bg-[#20C997] text-white rounded">Button 3</button>
+        </div>
+      </div>
+    </div>
   )
 }
 
