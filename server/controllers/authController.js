aa931fd15f88eb977436b9b1f4ac0f2e1236c639
fahim.hadi <fahimhadimaula@gmail.com>
2024-09-11 19:42:20 +0700

    server: backend in register page

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,9 +1,49 @@
-
+const User = require('../models/User')
 
 const test = (req, res) => {
   res.json('test is working')
 }
 
+const registerUser = async (req, res) => {
+  try {
+    const { email, username, password, confirmPassword } = req.body;
+    // check if username was entered
+    if(!username) {
+      return res.json({ error: 'Username is required' })
+    }
+
+    // check if email was entered
+    if(!email) {
+      return res.json({ error: 'Email is required' })
+    }
+
+    // check if password is good
+    if(!password || password.length < 6) {
+      return res.json({ error: 'Password is required and should be 6 characters long' })
+    }
+
+    // check if password and confirmPassword match
+    if(password !== confirmPassword) {
+      return res.json({ error: 'Passwords do not match' })
+    }
+
+    // check email is already exists or not
+    const exist = await User.findOne({email});
+    if(exist) {
+      return res.json({ error: 'Email already exists' })
+    }
+
+    const user = await User.create({
+      username, email, password
+    })
+
+    return res.json(user)
+  } catch (error) {
+    console.log(error)
+  }
+}
+
 module.exports = {
-  test
+  test,
+  registerUser
 }
--- server/index.js
+++ server/index.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
 const cors = require('cors');
+const { mongoose } = require('mongoose')
 
 const app = express();
+
+// connect mongodb
+mongoose.connect(process.env.MONGO_URL)
+  .then(() => console.log('MongoDB connected'))
+  .catch((err) => console.log('MongoDB is not connected', err));
+
+// middleware
+app.use(express.json());
 
 app.use('/', require('./routes/authRoutes'));
 
