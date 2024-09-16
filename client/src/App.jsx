e8b6f7049ec00babc776692656c9f8cdcdc487e8
fahim.hadi <fahimhadimaula@gmail.com>
2024-09-16 14:05:51 +0700

    server: created user context

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,20 +5,21 @@ import Register from './pages/Register'
 import Login from './pages/Login'
 import axios from 'axios'
 import { Toaster } from 'react-hot-toast'
+import { UserContextProvider } from '../context/userContext'
 
 axios.defaults.baseURL = 'http://localhost:8000'
 axios.defaults.withCredentials = true
 
 function App() {
   return (
-    <>
+    <UserContextProvider>
       <Toaster position='bottom-right' toastOptions={{duration: 2000}} />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/register' element={<Register />} />
         <Route path='/login' element={<Login />} />
       </Routes>
-    </>
+    </UserContextProvider>
   )
 }
 
